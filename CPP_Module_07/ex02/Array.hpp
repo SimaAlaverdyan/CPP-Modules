5076bd22a1f56cee673831a55ef43d70b7dcbf87
Sima Alaverdyan <sim.alaverdyan.02@gmail.com>
2022-11-19 23:29:24 +0400

    cpp07

---
+++ CPP_Module_07/ex02/Array.hpp
@@ -0,0 +1,77 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <stdexcept>
+
+template    <typename T>
+class Array
+{
+    private:
+        T   *_arr;
+        unsigned int _size;
+    public:
+        Array();
+		Array(unsigned int n);
+		Array(const Array &copy);
+		~Array();
+		
+        Array           &operator=(const Array &copy);
+        T               &operator[](unsigned int i) const;
+        unsigned int    size() const;
+};
+
+template    <typename T>
+Array<T>::Array() : _arr(0), _size(0) {}
+
+template    <typename T>
+Array<T>::Array(unsigned int n) : _arr(new T[n]), _size(n) {}
+
+template    <typename T>
+Array<T>::Array(const Array &copy) : _arr(0), _size(0) 
+{
+    if (_size != 0)
+	{
+		_arr = new T[_size];
+		for	(unsigned int i = 0; i < copy._size; ++i)
+			this->_arr[i] = copy._arr[i];
+	}
+}
+
+template	<typename T>
+Array<T>::~Array()
+{
+	delete [] this->_arr;
+}
+
+template	<typename T>
+Array<T>	&Array<T>::operator=(const Array &rhs)
+{
+	if (this != &rhs)
+	{
+		if (this->_size > 0)
+			delete [] this->_arr;
+
+		this->_size = rhs._size;
+		this->_arr = new T[_size];
+		for (unsigned int i = 0; i < rhs._size; ++i)
+			this->_arr[i] = rhs._arr[i];
+	}
+
+	return (*this);
+}
+
+template	<typename T>
+T			&Array<T>::operator[](unsigned int idx) const
+{
+	if (idx < 0 || idx >= this->_size)
+		throw std::range_error("index out of bounds");
+	return (this->_arr[idx]);
+}
+
+template		<typename T>
+unsigned int	Array<T>::size() const
+{
+	return (this->_size);
+}
+
+#endif
